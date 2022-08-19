77fc310ea48525b4b471db71fbb860cb738384b8
Software Engineer <97345885+Gangstar0312@users.noreply.github.com>
2022-08-19 17:51:37 +0200

    refactoring team mates

--- src/components/landingpage/Team.js
+++ src/components/landingpage/Team.js
@@ -13,7 +13,8 @@ import alex from '../../assets/images/ladingpage/team/alex.png';
 import sava from '../../assets/images/ladingpage/team/sava.png';
 import suedo from '../../assets/images/ladingpage/team/suedo.png';
 import zoro from '../../assets/images/ladingpage/team/zoro.png';
-
+import Kool_kid from '../../assets/images/ladingpage/team/Koolkid.jpeg';
+import ZeRoxx from '../../assets/images/ladingpage/team/ZeRoxx.jpeg';
 
 function Team() {
   return (
@@ -21,67 +22,67 @@ function Team() {
       <h2 className='sectionHeading text-center mt-5'>MEET OUR TEAM</h2>
       <div className='team d-flex flex-wrap justify-content-center'>
         <div className='team__card'>
-          <Image src={akeem}/>
+          <Image src={akeem} />
           <h4 className='team__heading'>Akeem</h4>
           <h4 className='team__heading--sub'>Creator</h4>
         </div>
         <div className='team__card'>
-          <Image src={obelisxx}/>
+          <Image src={obelisxx} />
           <h4 className='team__heading'>Obelisxx</h4>
           <h4 className='team__heading--sub'>Artist</h4>
         </div>
         <div className='team__card'>
-          <Image src={musto}/>
+          <Image src={musto} />
           <h4 className='team__heading'>Musto</h4>
           <h4 className='team__heading--sub'>Developer</h4>
         </div>
         <div className='team__card'>
-          <Image src={koki}/>
+          <Image src={koki} />
           <h4 className='team__heading'>Koki</h4>
           <h4 className='team__heading--sub'>Concept art</h4>
         </div>
         <div className='team__card'>
-          <Image src={jaarth}/>
+          <Image src={jaarth} />
           <h4 className='team__heading'>Jaarth</h4>
           <h4 className='team__heading--sub'>BearX Writer</h4>
         </div>
         <div className='team__card'>
-          <Image src={Koolz}/>
-          <h4 className='team__heading'>Koolz</h4>
-          <h4 className='team__heading--sub'>COMMUNITY LEADER</h4>
+          <Image src={ZeRoxx} style={{ borderRadius: "20px" }} />
+          <h4 className='team__heading'>ZeRoxx</h4>
+          <h4 className='team__heading--sub'>Head Mod</h4>
         </div>
         <div className='team__card'>
-          <Image src={Bainsy}/>
+          <Image src={Bainsy} />
           <h4 className='team__heading'>Bainsy</h4>
-          <h4 className='team__heading--sub'>COMMUNITY LEADER</h4>
+          <h4 className='team__heading--sub'>Mod</h4>
         </div>
         <div className='team__card'>
-          <Image src={Aymeezy}/>
-          <h4 className='team__heading'>Aymeezy</h4>
-          <h4 className='team__heading--sub'>COMMUNITY LEADER</h4>
+          <Image src={Kool_kid} style={{ borderRadius: "20px" }} />
+          <h4 className='team__heading'>0xkoolkid</h4>
+          <h4 className='team__heading--sub'>Mod</h4>
         </div>
         <div className='team__card'>
-          <Image src={zoro}/>
+          <Image src={zoro} />
           <h4 className='team__heading'>Zoro</h4>
-          <h4 className='team__heading--sub'>COMMUNITY LEADER</h4>
+          <h4 className='team__heading--sub'>Mod</h4>
         </div>
         <div className='team__card'>
-          <Image src={longed}/>
+          <Image src={longed} />
           <h4 className='team__heading'>Longed</h4>
           <h4 className='team__heading--sub'>Social</h4>
         </div>
         <div className='team__card'>
-          <Image src={suedo}/>
+          <Image src={suedo} />
           <h4 className='team__heading'>Suedo</h4>
           <h4 className='team__heading--sub'>Design</h4>
         </div>
         <div className='team__card'>
-          <Image src={alex}/>
+          <Image src={alex} />
           <h4 className='team__heading'>Alex</h4>
           <h4 className='team__heading--sub'>Design</h4>
         </div>
         <div className='team__card'>
-          <Image src={sava}/>
+          <Image src={sava} />
           <h4 className='team__heading'>Sava</h4>
           <h4 className='team__heading--sub'>Developer</h4>
         </div>
